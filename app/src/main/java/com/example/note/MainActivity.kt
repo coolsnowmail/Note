05b9f73acffb8196985fdb8e7ce98ec2d98e6838
mosen <cool.snow@mail.ru>
2024-07-23 21:57:45 +0300

    simple note lesson 3 done

--- app/src/main/java/com/example/note/MainActivity.kt
+++ app/src/main/java/com/example/note/MainActivity.kt
@@ -12,33 +12,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.note.databinding.ActivityMainBinding
 import com.example.note.db.MyDBManager
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var fbNewButton: View
-
-    //    lateinit var edTitle: EditText
-//    lateinit var edContent: EditText
-//    lateinit var tvText: TextView
+    lateinit var binding: ActivityMainBinding
     val myDBManager = MyDBManager(this)
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        fbNewButton = findViewById(R.id.fbNewButton)
-//        edTitle = findViewById(R.id.edTitle)
-//        edContent = findViewById(R.id.edContent)
-//        tvText = findViewById(R.id.tvText)
 
-        fbNewButton.setOnClickListener {
+        binding.fbNewButton.setOnClickListener {
             val i = Intent(this, EditActivity::class.java)
             startActivity(i)
         }
